91d83f100baa1b5857d9a32a10f21486141d2ddf
unknown <brandonhszzz@gmail.com>
2021-12-03 01:02:16 -0600

    Models and DB created

--- gestionPedidos/models.py
+++ gestionPedidos/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Clients(models.Model):
+  name = models.CharField(max_length = 30)
+  direction = models.CharField(max_length = 50)
+  email = models.EmailField(max_length = 50)
+  phone = models.CharField(max_length = 10)
+
+class Items(models.Model):
+  name = models.CharField(max_length = 30)
+  section = models.CharField(max_length = 20)
+  price = models.IntegerField()
+
+class Order(models.Model):
+  orderId = models.IntegerField()
+  date = models.DateField()
+  finished = models.BooleanField()

3e76864cd3f947d8f6032cedc60c7e650af1ec74
unknown <brandonhszzz@gmail.com>
2021-12-17 15:01:41 -0600

    formated query on models

--- gestionPedidos/models.py
+++ gestionPedidos/models.py
@@ -7,15 +7,24 @@ class Clients(models.Model):
   email = models.EmailField(max_length = 50)
   phone = models.CharField(max_length = 10)
 
+  def __str__(self):
+    return f'name: {self.name} - direction: {self.direction} - email: {self.email} - phone: {self.phone}'
+
 class Items(models.Model):
   name = models.CharField(max_length = 30)
   section = models.CharField(max_length = 20)
   price = models.IntegerField()
 
   def __str__(self):
-    return f'name: {self.name} \n - section: {self.section} \n - price: {self.price}\n'
+    return f'name: {self.name} - section: {self.section} - price: {self.price}\n'
 
 class Order(models.Model):
   orderId = models.IntegerField()
   date = models.DateField()
   finished = models.BooleanField()
+
+  def __str__(self):
+    return f'orderId: {self.orderId} - date: {self.date} - finished: {self.finished}\n'
+
+  def __str__(self):
+    return f'name: {self.name} - section: {self.section} - price: {self.price}\n'

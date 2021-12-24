715d0707e9c5555d0b15fea8184f349b82975f03
unknown <brandonhszzz@gmail.com>
2021-12-23 20:44:29 -0600

    models registed on cpanel

--- gestionPedidos/admin.py
+++ gestionPedidos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from gestionPedidos.models import Clients, Items, Order
 
-# Register your models here.
+# Register your models here
+admin.site.register(Clients)
+admin.site.register(Items)
+admin.site.register(Order)
--- gestionPedidos/models.py
+++ gestionPedidos/models.py
@@ -4,10 +4,10 @@ from django.db import models
 class Clients(models.Model):
   name = models.CharField(max_length = 30)
   direction = models.CharField(max_length = 50)
-  email = models.EmailField(max_length = 50)
+  email = models.EmailField(max_length = 50, blank=True, null=True)
   phone = models.CharField(max_length = 10)
 
-  def __str__(self):
+  def __str__(self) -> str:
     return f'name: {self.name} - direction: {self.direction} - email: {self.email} - phone: {self.phone}'
 
 class Items(models.Model):
@@ -15,12 +15,12 @@ class Items(models.Model):
   section = models.CharField(max_length = 20)
   price = models.IntegerField()
 
-  def __str__(self):
+  def __str__(self) -> str:
     return f'name: {self.name} - section: {self.section} - price: {self.price}'
 class Order(models.Model):
   orderId = models.IntegerField()
   date = models.DateField()
   finished = models.BooleanField()
 
-  def __str__(self):
+  def __str__(self) -> str:
     return f'orderId: {self.orderId} - date: {self.date} - finished: {self.finished}'
